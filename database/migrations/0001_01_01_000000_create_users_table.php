6e2e84726f9c4cad5d596b41edd861106fc973dd
MUHAMMADFIRDASATRIA <satriajjn@students.unnes.ac.id>
2026-02-24 23:16:38 +0700

    feat: add migrations for departments, positions, employees, and pkwt tables

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+
 return new class extends Migration
 {
     /**
@@ -15,6 +16,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->enum('role', ['admin', 'hrd'])->default('hrd');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
---
+++ database/migrations/2026_02_24_161318_create_pkwt.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pkwt', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('employee_id')->constrained('positions')->onDelete('cascade');
+            $table->string('contract_number')->unique();
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->enum('status', ['active', 'near-expired', 'expired'])->default('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pkwt');
+    }
+};
